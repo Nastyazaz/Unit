910b75a458f972feecbf4f4c1d9bc1dc74791ab4
N <dynaeva.nastya96@gmail.com>
2024-11-24 23:26:58 +0300

    two

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,29 +4,33 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesTwo() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesThree() {
-        org.testng.Assert.assertEquals(service.remain(1), 999);
+        org.junit.Assert.assertEquals(service.remain(1), 999);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesFour() {
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+        org.junit.Assert.assertEquals(service.remain(999), 1);
     }
 
+    @org.junit.Test
+    public void testRemainBoundaryValuesFive() {
+        org.junit.Assert.assertEquals(service.remain(1000), 0);
+    }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesSix() {
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
+        org.junit.Assert.assertEquals(service.remain(1001), 999);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainCriticalPath() {
-        org.testng.Assert.assertEquals(service.remain(400), 600);
+        org.junit.Assert.assertEquals(service.remain(400), 600);
     }
 }
